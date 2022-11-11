253dcc6108c4b2d97184390d4dfb8b24df5f379c
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-11-11 14:48:18 +0000

    handling input of two fields

--- components/postForm.tsx
+++ components/postForm.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { KeyboardEventHandler } from 'react';
 
-function PostForm() {
+type Props = {
+    inputHandler: (
+        e:
+            | React.ChangeEvent<HTMLInputElement>
+            | React.ChangeEvent<HTMLTextAreaElement>
+    ) => any;
+    submitHandler: React.FormEventHandler;
+};
+
+function PostForm({ inputHandler, submitHandler }: Props) {
     return (
-    <div className="block p-6 rounded-lg shadow-lg bg-white max-w-md">
-  <form>
-    <div className="form-group mb-6">
-      <input type="text" className="form-control block
+        <div className='block p-6 rounded-lg shadow-lg bg-white max-w-md'>
+            <form onSubmit={submitHandler}>
+                <div className='form-group mb-6'>
+                    <input
+                        onChange={(e) => {
+                            inputHandler(e);
+                        }}
+                        name='title'
+                        type='text'
+                        className='form-control block
         w-full
         px-3
         py-1.5
@@ -18,12 +33,18 @@ function PostForm() {
         transition
         ease-in-out
         m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
-        placeholder="Title"></input>
-    </div>
-    <div className="form-group mb-6">
-      <textarea
-      className="
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+                        id='exampleInput7'
+                        placeholder='Title'
+                    ></input>
+                </div>
+                <div className='form-group mb-6'>
+                    <textarea
+                        onChange={(e) => {
+                            inputHandler(e);
+                        }}
+                        name='description'
+                        className='
         form-control
         block
         w-full
@@ -39,17 +60,21 @@ function PostForm() {
         ease-in-out
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      "
-      id="exampleFormControlTextarea13"
-      placeholder="Message"
-    ></textarea>
-    </div>
-    <div className="form-group form-check text-center mb-6">
-      <input type="file"
-        // className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
-        id="exampleCheck87"></input>
-    </div>
-    <button type="submit" className="
+      '
+                        id='exampleFormControlTextarea13'
+                        placeholder='Message'
+                    ></textarea>
+                </div>
+                <div className='form-group form-check text-center mb-6'>
+                    <input
+                        type='file'
+                        // className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+                        id='exampleCheck87'
+                    ></input>
+                </div>
+                <button
+                    type='submit'
+                    className='
       w-full
       px-6
       py-2.5
@@ -66,10 +91,13 @@ function PostForm() {
       active:bg-black active:shadow-lg
       transition
       duration-150
-      ease-in-out">Submit</button>
-  </form>
-</div>
-    )
+      ease-in-out'
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default PostForm;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -7,43 +8,60 @@ import PostForm from '../components/postForm';
 import styles from '../styles/Home.module.css';
 
 type POST = {
-  data: [
-    {
-      title: string;
-      description: string;
-      image: string;
-    }
-  ];
+    data: [
+        {
+            title: string;
+            description: string;
+            image: string;
+        }
+    ];
 };
 
 export default function Home(props: POST) {
-  console.log('index props', props);
-  return (
-    <div className='flex justify-center flex-col'>
-      <h1 className='text-center'>Main Page</h1>
-      <div className='flex justify-center mt-4'>
-        <PostForm />
-      </div>
-      <div className='grid md:grid-cols-3 grid-cols-1 gap-4 mt-4'>
-        {props.data.map((post, index) => {
-          return (
-            <Card
-              key={index}
-              title={post.title}
-              description={post.description}
-              image={post.image}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+    const [inputs, setInputs] = useState({ title: '', description: '' });
+    function inputHandler(
+        e:
+            | React.ChangeEvent<HTMLInputElement>
+            | React.ChangeEvent<HTMLTextAreaElement>
+    ) {
+        console.log({ [e.target.name]: e.target.value });
+        setInputs({ ...inputs, [e.target.name]: e.target.value });
+    }
+
+    function submitHandler(e: React.SyntheticEvent) {
+        e.preventDefault();
+        console.log(inputs);
+    }
+
+    return (
+        <div className='flex justify-center flex-col'>
+            <h1 className='text-center'>Main Page</h1>
+            <div className='flex justify-center mt-4'>
+                <PostForm
+                    inputHandler={inputHandler}
+                    submitHandler={submitHandler}
+                />
+            </div>
+            <div className='grid md:grid-cols-3 grid-cols-1 gap-4 mt-4'>
+                {props.data.map((post, index) => {
+                    return (
+                        <Card
+                            key={index}
+                            title={post.title}
+                            description={post.description}
+                            image={post.image}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const res = await fetch('http://localhost:3000/api/posts');
-  const data = await res.json();
-  return {
-    props: { data }, // will be passed to the page component as props
-  };
+    const res = await fetch('http://localhost:3000/api/posts');
+    const data = await res.json();
+    return {
+        props: { data }, // will be passed to the page component as props
+    };
 };
